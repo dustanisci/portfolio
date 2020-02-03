60b00cd147c98e73318e1eae70713e7d75e7192c
Eduardo Stanisci <dustanisci@outlook.com>
2020-02-03 00:04:15 -0300

    animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@shared/models/loader';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +12,8 @@ export class AppComponent implements OnInit {
 
   public multiloader: Loader = {} as Loader;
 
-  ngOnInit(): void {
+  ngOnInit() {
+    AOS.init();
     this.multiloader.about = true;
     this.multiloader.portfolio = true;
     this.multiloader.skills = true;
