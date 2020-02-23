1f76492f05c46630c2e8d0c2fe7f5d0905e4f7e7
Eduardo <eduardo.stanisci@omnibees.com>
2020-02-23 15:43:40 -0300

    updated translations.

--- src/app/shared/api/api.service.ts
+++ src/app/shared/api/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpHeaders, HttpParams, HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -12,12 +12,33 @@ export class ApiService {
 
   constructor(private http: HttpClient) { }
 
-  public get(url, request?): Observable<Object> {
+  private setHeaders(request?): void {
     if (request) {
+      this.request = {} as HttpParams;
       Object.keys(request).map(key => {
         this.request.append(key, request[key]);
       });
     }
-    return this.http.get(url, { headers: this.headers, params: this.request });
   }
+
+  public get(url: string, request?): Observable<any> {
+    this.setHeaders(request);
+    return this.http.get<any>(url, { headers: this.headers, params: this.request });
+  }
+
+  public post(url: string, value: any, request?): Observable<any> {
+    this.setHeaders(request);
+    return this.http.post<any>(url, value, { headers: this.headers, params: this.request });
+  }
+
+  public put(url: string, id: any, value: any, request?): Observable<any> {
+    this.setHeaders(request);
+    return this.http.put(`${url}/${id}`, value, { headers: this.headers, params: this.request });
+  }
+
+  public delete(url: string, id: any, request?): Observable<any> {
+    this.setHeaders(request);
+    return this.http.delete(`${url}/${id}`, { headers: this.headers, params: this.request });
+  }
+
 }
