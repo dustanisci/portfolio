df1dddc3d92966495001e316f129162d75778566
Eduardo <dustanisci@outlook.com>
2021-07-09 16:19:42 -0300

    build: environments adjustment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,8 @@ export const environment = {
 };
 
 export const endpoint = {
-  skills: 'https://www.mocky.io/v2/5e337f553000008300d95f36',
-  navbar: 'https://www.mocky.io/v2/5e25127a2f00008600ce2794',
-  about: 'https://www.mocky.io/v2/5e335317320000bae294d524',
-  portfolio: 'https://www.mocky.io/v2/5e3434273000008245d9637d'
+  skills: 'https://raw.githubusercontent.com/peditapronto/backup-image/master/portfolioOld/skills.json',
+  navbar: 'https://raw.githubusercontent.com/peditapronto/backup-image/master/portfolioOld/navbar.json',
+  about: 'https://raw.githubusercontent.com/peditapronto/backup-image/master/portfolioOld/about.json',
+  portfolio: 'https://raw.githubusercontent.com/peditapronto/backup-image/master/portfolioOld/portfolio.json'
 };
